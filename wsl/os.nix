c8f4531437bef82cd87abbe8b3f2a014076f2dc1
Giacomo Fraron <giacomo.fraron@avvale.com>
2026-04-23 11:02:59 +0200

    Test per network su WSL

--- wsl/os.nix
+++ wsl/os.nix
@@ -9,8 +9,8 @@
   ];
   # WSL
   # wsl.wslConf.network.generateHosts = false;      # cat /etc/hosts
-  # wsl.wslConf.network.generateResolvConf = false; # cat /etc/resolv.conf
-  # networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  wsl.wslConf.network.generateResolvConf = false; # cat /etc/resolv.conf
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" "10.255.255.254" ];
   # Docker
   virtualisation.docker.enable = true;
   # App
